f0b73c0c5bc0d5b29b09f47d777d2339574015a4
Kacper Dybowski <s155380@o365.student.pg.edu.pl>
2023-04-24 12:13:30 +0200

    Refactoring GlobalConfig and CreatePrizeForm

---
+++ TrackerLibrary/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+
+    public enum DatabaseType
+    {
+        Sql,
+        TextFile
+    }
+
+}
