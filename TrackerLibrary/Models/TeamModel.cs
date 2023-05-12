583444d1342e01538f16e4434290e1188e7334fe
Kacper Dybowski <s155380@o365.student.pg.edu.pl>
2023-05-12 11:59:13 +0200

    Create a method for create team in CreateTeamForm

--- TrackerLibrary/Models/TeamModel.cs
+++ TrackerLibrary/Models/TeamModel.cs
@@ -10,15 +10,21 @@ namespace TrackerLibrary.Models
     public class TeamModel
     {
 
+
+        /// <summary>
+        /// The uniqe identifier for the Team in database.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Represent the name of the team.
+        /// </summary>
+        public string TeamName { get; set; }
+
         /// <summary>
         /// Represents members of one team.
         /// </summary>
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
 
-        /// <summary>
-        /// Represent the name of the team.
-        /// </summary>
-        public string TeamName { get; set; }
 
     }
 
